8c348f9520d7603d9b558041f77d87c6e7360233
eshita <eshitachakrabarti9@gmail.com>
2021-12-24 20:54:22 +1100

    basic control flow solved

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+ if (distance <= 400 ){
+   return'This one is on me!'
+  } else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500){
+    return 'No can do.'
+  
+  }
+}
+
+
+
+  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipString){
+  if (tipString === "generous"){
+    return "Thank you so much."
 
-function switchOnCharmFromTip(){
+  } else if (tipString === "not as generous"){
+    return "Thank you."
+
+  } else if (tipString === "thanks for everything"){
+    return "Bye."
+
+  }
+  
   // Write your code here!
 }
